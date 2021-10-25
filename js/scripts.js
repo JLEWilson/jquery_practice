f172d12116724fff46fed4010e6633eac5904ebf
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-10-25 12:05:12 -0800

    Added variable to the for each function so index doesnt mess up the click
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
   const elementsArr = ["h1", "p", "img"];
   const messageArr = ["This is a heading.", "This is a paragraph.", "This is a image."];
   let index = 0;
-  let currentIndex = [];
 
   elementsArr.forEach(function(element){
+    let num = index; // Defining a variably ONLY within this iteration of the function that equals index
     $(elementsArr[index]).click(function(){
-      alert(messageArr[index]);
+      alert(messageArr[num]); // Assigning the int that is only in this function as the index
     });
     index += 1;
   });
